76e255efbb3a3f2d63738115b0a0543fccb2293f
AJONESEG <91177956+AJONESEG@users.noreply.github.com>
2022-06-05 13:30:01 -0700

    Add files via upload

--- CS350FinalProject/src/cs350s22/component/ui/parser/createFunctions.java
+++ CS350FinalProject/src/cs350s22/component/ui/parser/createFunctions.java
@@ -18,12 +18,27 @@ public class createFunctions {
 	}
 
 	public static void createActuator (String[] leftOver){
-		if(leftOver[1].equalsIgnoreCase("LINEAR")){
+		String type = null;
+		String actID = null;
+		double leadin, leadout, relax;
+		int vL, vMin, vMax, initial, jerkLimit;
+		if(leftOver[2].equalsIgnoreCase("LINEAR") || leftOver[2].equalsIgnoreCase("ROTARY")){
+			type = leftOver[2]; // gets the type of actuator
+			actID = leftOver[3]; // gets the name of the actuatoc
+		}
+		if(leftOver[4].equalsIgnoreCase("ACCELERATION")) {
+			leadin =  Double.parseDouble(leftOver[6].trim()); // converting leading value to double
+			leadout =  Double.parseDouble(leftOver[8].trim()); // converting leading value to double
+			relax =  Double.parseDouble(leftOver[10].trim()); // gettin relax value
+			vL =  Integer.parseInt(leftOver[13].trim()); // gettin velocity limit
+			vMin = Integer.parseInt(leftOver[16].trim());
+			vMax = Integer.parseInt(leftOver[18].trim());
+			initial = Integer.parseInt(leftOver[20].trim());
+			jerkLimit = Integer.parseInt(leftOver[23].trim());
 
 		}
-		if(leftOver[1].equalsIgnoreCase("ROTARY")){
 
-		}
+
 
 	}
 
