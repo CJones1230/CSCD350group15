e40a840f48eff7746f209f090699312187a7b9ac
BenFristad <bpfristad@gmail.com>
2022-06-06 15:01:21 -0700

    Ben - Almost Finished [NEED TO SET ID]

--- CS350FinalProject/src/cs350s22/component/ui/parser/ParseWatchdog.java
+++ CS350FinalProject/src/cs350s22/component/ui/parser/ParseWatchdog.java
@@ -1,27 +1,119 @@
 package cs350s22.component.ui.parser;
 
+import cs350s22.component.sensor.watchdog.*;
+import cs350s22.component.sensor.watchdog.mode.*;
+
 public class ParseWatchdog {
     public static void createWatchdog(String[] command) {
 
-        String type;
-        String mode;
+        A_Watchdog watchdog;
+        A_WatchdogMode mode;
+        double thresholdLow;
+        double thresholdHigh;
+        double threshold;
+        int complianceFailureThreshold;
+        boolean grace = false;
 
-        for (int i = 3; i < command.length - 3; i++) {
+        for (int i = 4; i < command.length - 4; i++) {
             switch (command[i]) {
 
                 case "mode":
-                    mode = command[i] + 1;// FIX
+                    switch(command[i + 1]) {
+
+                        case "instantaneous": mode = new WatchdogModeInstantaneous();
+                            break;
+
+                        case "average": mode = new WatchdogModeAverage();
+                            break;
+
+                        case "standard": mode = new WatchdogModeStandardDeviation();
+                            break;
+
+                        default:
+                            System.out.println("Please Enter a Valid Mode.");
+
+                    }// end nested switch
+                    break;
 
                 case "threshold":
+                    switch(command[i + 1]) {
+
+                        case "low":
+                            thresholdLow = Double.valueOf(command[i + 2]);
+                            thresholdHigh = Double.valueOf(command[i + 4]);
+                            break;
+
+                        case "high":
+                            thresholdHigh = Double.valueOf(command[i + 2]);
+                            thresholdLow = Double.valueOf(command[i + 4]);
+                            break;
+
+                        default:
+                            if (command[i + 1].matches("-?\\d+(\\.\\d+)?") == true) { // check if value is a double
+                                threshold = Double.valueOf(command[i + 2]);
+                            } else {
+                                System.out.println("Please Enter a Valid Threshold.");
+                            }
+                    }// end nested switch
                     break;
 
-                case "grace":
+                case "grace": complianceFailureThreshold = Integer.valueOf(command[i + 1]);
+                     grace = true; // grace value is set
                     break;
-
 
             }// end switch
 
         }// end for loop
+
+        switch(command[2]) {
+
+            case "acceleration":
+                if(grace) {
+                    watchdog = new WatchdogAcceleration(thresholdLow, thresholdHigh, mode, complianceFailureThreshold);
+                }
+                else {
+                    watchdog = new WatchdogAcceleration(thresholdLow,thresholdHigh, mode);
+                }
+                break;
+
+            case "band":
+                if(grace) {
+                    watchdog = new WatchdogBand(thresholdLow, thresholdHigh, mode, complianceFailureThreshold);
+                }
+                else {
+                    watchdog = new WatchdogBand(thresholdLow,thresholdHigh, mode);
+                }
+                break;
+
+            case "notch":
+                if(grace) {
+                    watchdog = new WatchdogNotch(thresholdLow, thresholdHigh, mode, complianceFailureThreshold);
+                }
+                else {
+                    watchdog = new WatchdogNotch(thresholdLow,thresholdHigh, mode);
+                }
+                break;
+
+            case "low":
+                if(grace) {
+                    watchdog = new WatchdogLow(threshold, mode, complianceFailureThreshold);
+                }
+                else {
+                    watchdog = new WatchdogLow(threshold, mode);
+                }
+                break;
+
+            case "high":
+                if(grace) {
+                    watchdog = new WatchdogHigh(threshold, mode, complianceFailureThreshold);
+                }
+                else {
+                    watchdog = new WatchdogHigh(threshold, mode);
+                }
+                break;
+
+            default: System.out.println("Please Enter a Valid Type.");
+        }// end switch
 
     }// end method
 
