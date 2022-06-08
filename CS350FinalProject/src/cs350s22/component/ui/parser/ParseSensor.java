ae7b0a86b11841c20b04a3657c9a2b608c83c4c7
BenFristad <bpfristad@gmail.com>
2022-06-07 22:46:47 -0700

    Ben - In Progress

--- CS350FinalProject/src/cs350s22/component/ui/parser/ParseSensor.java
+++ CS350FinalProject/src/cs350s22/component/ui/parser/ParseSensor.java
@@ -1,7 +1,13 @@
 package cs350s22.component.ui.parser;
 
+import cs350s22.component.sensor.A_Sensor;
+import cs350s22.support.Identifier;
+
 public class ParseSensor {
-    public static void createSensor(String[] command) {
+    public static void createSensor(String[] command, A_ParserHelper parserHelper) {
+
+        A_Sensor sensor = null;
+        Identifier sensorID = Identifier.make(command[3]);
 
         for (int i = 4; i < command.length - 4; i++) {
             switch (command[i]) {
@@ -21,6 +27,8 @@ public class ParseSensor {
             }// end switch
 
         }// end for loop
+
+        parserHelper.getSymbolTableSensor().add(sensorID, sensor);  // store ID in symbol table
 
     }// end method
 
