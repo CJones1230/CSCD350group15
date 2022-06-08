c6c7a3f5b6b978b1cb0d7f4240a4b30c75cc0e04
BenFristad <bpfristad@gmail.com>
2022-06-07 18:13:04 -0700

    Ben - Almost Finished [NEED TO SET ID AND FINISH NOTIFY

--- CS350FinalProject/src/cs350s22/component/ui/parser/ParseReporter.java
+++ CS350FinalProject/src/cs350s22/component/ui/parser/ParseReporter.java
@@ -1,8 +1,82 @@
 package cs350s22.component.ui.parser;
 
+import cs350s22.component.sensor.reporter.A_Reporter;
+import cs350s22.component.sensor.reporter.ReporterChange;
+import cs350s22.component.sensor.reporter.ReporterFrequency;
+import cs350s22.support.Identifier;
+import java.util.List;
+
 public class ParseReporter {
-    public static void createReporter(String[] leftOver){
+    public static void createReporter(String[] command){
+
+        A_Reporter reporter;
+        List<Identifier> ids = null;
+        List<Identifier> groups = null;
+        boolean isChange = false;
+        boolean isFrequency = false;
+        int reportingFrequency = 0;
+        int deltaThreshold = 0;
+
+        for (int i = 4; i < command.length - 4; i++) {
+            switch (command[i]) {
+
+                case "delta":
+                    isChange = true; // set change flag
+                    deltaThreshold = Integer.valueOf(command[i + 1]);
+                    break;
+
+                case "frequency":
+                    isFrequency = true; // set frequency flag
+                    reportingFrequency = Integer.valueOf(command[i + 1]);
+                    break;
+
+                case "notify":
+                    int j = i + 2;
+                    if (command[i + 1].equals("ids")) {
+
+                        while(command[j] != "groups" || command[j] != "frequency" || command[j] != "delta") {
+
+                            Identifier id = null;
+                            id.make(command[j]);
 
 
-    }
-}
+                        }// end while loop
+
+                    }
+                    else if(command[i + 1].equals("groups")) {
+                        while(command[j] != "ids" || command[j] != "frequency" || command[j] != "delta") {
+
+                            Identifier id = null;
+                            id.make(command[j]);
+
+
+                        }// end while loop
+                    }
+                    else {
+                        System.out.println("Please Enter Valid Command.");
+                    }
+                    break;
+
+            }// end switch
+
+        }// end for loop
+
+        if(isChange && groups.isEmpty()) {
+            reporter = new ReporterChange(ids, deltaThreshold);
+        }
+        else if(isChange) {
+            reporter = new ReporterChange(ids, groups, deltaThreshold);
+        }
+        else if(isFrequency && groups.isEmpty()) {
+            reporter = new ReporterFrequency(ids, reportingFrequency);
+        }
+        else if(isFrequency) {
+            reporter = new ReporterFrequency(ids, groups, reportingFrequency);
+        }
+        else {
+            System.out.println("Please Enter Valid Type.");
+        }
+
+    }// end method
+
+}// end class
