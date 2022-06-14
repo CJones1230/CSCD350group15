22d8ad935924546e620a54105d02e409ed35eab2
BenFristad <bpfristad@gmail.com>
2022-06-13 20:45:34 -0700

    complete

---
+++ CS350FinalProject/src/cs350s22/component/ui/parser/ParseGet.java
@@ -0,0 +1,16 @@
+package cs350s22.component.ui.parser;
+
+public class ParseGet {
+
+    public static void initialGet(String[] commandArray, A_ParserHelper parserHelper) {
+
+        switch(commandArray[1]) {
+
+            case "sensor": ParseSensor.getSensor(commandArray, parserHelper);
+                break;
+
+        }// end switch
+
+    }// end method
+
+}// end class
